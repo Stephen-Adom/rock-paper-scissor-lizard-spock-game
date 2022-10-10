8352fb8a37133b2db602494377f7d1abff17ab1e
Alaska94 <stephen.alaska94@gmail.com>
2022-10-10 14:32:20 +0000

    Header Score Updated

--- src/components/HeaderScore.tsx
+++ src/components/HeaderScore.tsx
@@ -47,7 +47,25 @@ export const HeaderScore = () => {
         <img src={Logo} className="w-[80%]" alt="logo" />
       </div>
 
-      <section className="flex items-center">{displayResult()}</section>
+      <section className="flex items-center sm:flex md:hidden">
+        {displayResult()}
+      </section>
+      <section className="items-center hidden sm:hidden md:flex">
+        <div className="score-section bg-deepBlue rounded-lg py-3 px-8 text-center mr-3">
+          <h6 className="text-white font-semibold">LOSE</h6>
+          <h1 className="font-bold text-textColor text-5xl">{loseScore}</h1>
+        </div>
+
+        <div className="score-section bg-deepBlue rounded-lg py-3 px-8 text-center mr-3">
+          <h6 className="text-white font-semibold">DRAW</h6>
+          <h1 className="font-bold text-textColor text-5xl">{drawScore}</h1>
+        </div>
+
+        <div className="score-section bg-white rounded-lg py-3 px-8 text-center">
+          <h6 className="text-scoreText font-semibold">SCORE</h6>
+          <h1 className="font-bold text-textColor text-5xl">{totalScore}</h1>
+        </div>
+      </section>
     </section>
   );
 };
